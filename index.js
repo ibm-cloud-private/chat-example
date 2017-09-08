b442abf8e8ca3b6507e4f35d6b65670c223dda1f
Jacky Huang <qhuang@au1.ibm.com>
2017-09-08 11:31:35 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var cowsay = require("cowsay");
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+
+  var msg1 = cowsay.say({
+	                  text : msg,
+	                  e : "oO",
+	                  T : "U "
+             });
+    
     io.emit('chat message', msg);
+    
+    io.emit('chat message', msg1);
+    
   });
 });
 
